1db02cd9b0c1eaa05d5a20b47432fa1fd6a3e3ce
ubuntumiki <ubuntu.miki@gmail.com>
2025-03-14 09:56:09 +0200

    create class Forward and fields

---
+++ src/com/example/practice/task1/Forward.java
@@ -0,0 +1,15 @@
+//Поля
+//Прізвище - Амплуа - Вік - Кількість ігор - Кількість голів
+//Визначити найкращого форварда, і вивести відомості про футболістів, які зіграли менше 5-ти ігор.
+
+package com.example.practice.task1;
+
+public class Forward {
+    private String surname;
+    private String role;
+    private int age;
+    private int gamesCount;
+    private int golCount;
+
+
+}
